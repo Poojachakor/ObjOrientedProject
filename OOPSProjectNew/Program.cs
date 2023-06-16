1902b77f810d9f7865b2efc64c111d7472aa78a8
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 22:52:52 +0530

    Add: UC3 file

--- OOPSProjectNew/Program.cs
+++ OOPSProjectNew/Program.cs
@@ -4,9 +4,34 @@
     {
         static void Main(string[] args)
         {
-            FindUniqueElement unique = new FindUniqueElement();
-            unique.CheckUniqueElement();
+            int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1 };
 
+            
+            Dictionary<int, int> frequencyDict = new Dictionary<int, int>();
+
+            
+            foreach (int element in arr)
+            {
+                
+                if (frequencyDict.ContainsKey(element))
+                {
+                    
+                    frequencyDict[element]++;
+                }
+                else
+                {
+                    
+                    frequencyDict[element] = 1;
+                }
+            }
+
+            // Display the frequency of each element
+            foreach (var kvp in frequencyDict)
+            {
+                Console.WriteLine("Element: {0}, Frequency: {1}", kvp.Key, kvp.Value);
+            }
         }
     }
+
 }
+    
