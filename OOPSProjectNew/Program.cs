2ed45beea58a28791729d53b1e3a3720cc63be60
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 23:19:49 +0530

    Add: UC9 file

--- OOPSProjectNew/Program.cs
+++ OOPSProjectNew/Program.cs
@@ -5,11 +5,35 @@
         static void Main(string[] args)
         {
 
-            SumOfAllDigits sumOfAllDigits = new SumOfAllDigits();
-            sumOfAllDigits.Sum();
+            int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
 
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[] rowSums = new int[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    sum += matrix[i, j];
+                }
+                rowSums[i] = sum;
+            }
+
+            Console.WriteLine("Sum of each row:");
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine($"Row {i + 1}: {rowSums[i]}");
+            }
         }
     }
+    
 }
 
     
