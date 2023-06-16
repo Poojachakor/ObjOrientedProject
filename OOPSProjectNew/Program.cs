706d44407c3022da60b16a32670c53ae71d3b027
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 22:58:19 +0530

    Add: UC4 file

--- OOPSProjectNew/Program.cs
+++ OOPSProjectNew/Program.cs
@@ -4,34 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1 };
+            FindMinMaxElement findMinMaxElement = new FindMinMaxElement();
+            findMinMaxElement.MinimumMaximumElement();
 
-            
-            Dictionary<int, int> frequencyDict = new Dictionary<int, int>();
-
-            
-            foreach (int element in arr)
-            {
-                
-                if (frequencyDict.ContainsKey(element))
-                {
-                    
-                    frequencyDict[element]++;
-                }
-                else
-                {
-                    
-                    frequencyDict[element] = 1;
-                }
-            }
-
-            // Display the frequency of each element
-            foreach (var kvp in frequencyDict)
-            {
-                Console.WriteLine("Element: {0}, Frequency: {1}", kvp.Key, kvp.Value);
-            }
         }
     }
-
 }
+
     
