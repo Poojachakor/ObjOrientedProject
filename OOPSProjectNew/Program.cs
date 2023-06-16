eb5e2f85485b3f15171931fe4a1ae9a8d4e158f5
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 23:08:57 +0530

    Add: UC6 File

--- OOPSProjectNew/Program.cs
+++ OOPSProjectNew/Program.cs
@@ -4,16 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int rows = 8;
 
-            for (int i = rows; i >= 1; i--)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            Pattern2 pattern = new Pattern2();
+            pattern.SquarePattern();
 
         }
     }
