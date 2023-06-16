a807c5e744dce0cf8650dd246a457366176aa2d9
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 22:47:54 +0530

    Add: UC2 file

--- OOPSProjectNew/Program.cs
+++ OOPSProjectNew/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            DuplicateElement duplicateElement = new DuplicateElement();
-            duplicateElement.CheckDuplicateItem();
+            FindUniqueElement unique = new FindUniqueElement();
+            unique.CheckUniqueElement();
+
         }
     }
 }
